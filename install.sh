15e1967014972c14bed4a0a5b676cf06e359d2ba
Pbezerra-dev <herbetyp@gmail.com>
2020-04-15 14:08:05 -0300

    update install script

--- install.sh
+++ install.sh
@@ -3,13 +3,11 @@
 # mtranscoder installation script
 
 # creates director Mtranscoder at / opt
-sudo mkdir /opt/Mtranscoder
+sudo mkdir -p /opt/Mtranscoder
 
 # Copy the files for folder /opt
 sudo cp -r libs /opt/Mtranscoder && sudo cp mtranscoder /opt/Mtranscoder
 
 # Create symbolic mtranscoder link to /usr/local/bin
-sudo ln -s /opt/Mtranscoder/mtranscoder /usr/local/bin
-
-# Create symbolic libs link to /usr/local/bin
-sudo ln -s /opt/Mtranscoder/libs /usr/local/bin
+sudo rm -f /usr/local/bin/mtranscoder
+sudo ln -s /opt/Mtranscoder/mtranscoder /usr/local/bin/mtranscoder
