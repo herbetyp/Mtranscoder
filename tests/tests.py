b9849cf8d3c0eb1448fd7090fba00f79c46d4ed6
Pbezerra-dev <herbetyp@gmail.com>
2020-04-16 01:16:12 -0300

    tests

---
+++ tests/tests.py
@@ -0,0 +1,23 @@
+import os
+from unittest import TestCase
+
+from libs.check_files import files_exists, file_exist
+from libs.args import args
+from libs.video import Video
+from libs.audio import Audio
+
+
+class CheckFilesTest(TestCase):
+    ...
+
+
+class VideoTest(TestCase):
+    ...
+
+
+class AudioTest(TestCase):
+    ...
+
+
+class ArgsTest(TestCase):
+    ...
