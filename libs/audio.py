885f99800782da0e151ae5fbee649d94ba0c04dd
Pbezerra-dev <herbetyp@gmail.com>
2020-05-07 00:28:14 -0300

    code improvements

--- libs/audio.py
+++ libs/audio.py
@@ -1,14 +1,12 @@
-from itertools import count
 import os
 
 from .video import Video
 from .check_files import files_exists
-from .progresss_bar import progressbar
 
 
 class Audio(Video):
-    def __init__(self, input_midia_format, source_path, output_midia_format,
-                 output_path, bitrate, time, qrange):
+    def __init__(self, input_midia_format: str, source_path: str, output_midia_format: str,
+                 output_path, bitrate: str, time: str, qrange: int) -> None:
         super().__init__(input_midia_format, source_path,
                          output_midia_format, output_path, bitrate, time, qrange)
 
@@ -37,33 +35,26 @@ class Audio(Video):
             self.bitrate_audio = bitrate + ' -f adts'
             self.time = time
 
-    def execute_files(self):
-        files = set((files for files in os.listdir(self.source_path)[:self.qrange]
-                     if self.input_midia_format in files.split('.')[-1]))
-        if files:
-            for file in files:
-                name_file, extension_file = os.path.splitext(file)
+    def filter_files(self):
+        files = list((files for files in os.listdir(self.source_path)
+                      if self.input_midia_format in files.split('.')[-1]))
+        if not files:
+            return False
+        return files[:self.qrange]
 
-                exit_file = (
-                    f'{self.output_path}/{name_file}_{self.output_midia_format}.'
-                    f'{self.output_midia_format}'
+    def processing_file(self, files):
+        for file in files:
+            name_file, _ = os.path.splitext(file)
+
+            exit_file = (
+                f'{self.output_path}/{name_file}_{self.output_midia_format}.'
+                f'{self.output_midia_format}'
+            )
+            if files_exists(name_file, exit_file, self.output_midia_format):
+                command = (
+                    f'{self.command_ffmpeg} -i "{self.source_path}/{file}" -vn '
+                    f'{self.bitrate_audio} {self.codec_audio} {self.time} '
+                    f'"{exit_file}" -y &> /dev/null'
                 )
-                if files_exists(name_file, exit_file, self.output_midia_format):
-                    command = (
-                        f'{self.command_ffmpeg} -i "{self.source_path}/{file}" -vn '
-                        f'{self.bitrate_audio} {self.codec_audio} {self.time} '
-                        f'"{exit_file}" -y &> /dev/null'
-                    )
-
-                    def ffmpeg():
-                        if os.system(command):
-                            for i in count(1):
-                                ...
-                        return
-
-                    progressbar(ffmpeg)
-                else:
-                    break
-        else:
-            print(
-                f'\nFiles with extension .{self.input_midia_format} in "{self.source_path}"')
+                self.execute(command)
+            continue
